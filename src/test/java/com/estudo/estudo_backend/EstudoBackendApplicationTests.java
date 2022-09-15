26e06b130bd0f99f7f5dbb409b099ac68f19193f
Costa <gustavo.oliva.costa@accenture.com>
2022-09-15 20:22:06 -0300

    Criando projeto com Spring Boot

---
+++ src/test/java/com/estudo/estudo_backend/EstudoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.estudo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
